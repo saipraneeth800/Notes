8d39c2631708bc39cd374a6310710fc6021a73ce
saipraneeth800 <praneethsai800@gmail.com>
2022-09-21 12:07:56 +0530

    final

--- pages/Notes.js
+++ pages/Notes.js
@@ -8,37 +8,72 @@ import {
 // import Cards from "./Cards";
 
 const Notes = () => {
+  // const ISSERVER = typeof window === "undefined";
   const [note, setNote] = useState({
     title: "",
     content: "",
   });
-  const getLocalInfo =() =>
-  {
-    if (typeof window !== 'undefined'){
+  const [toggleButton, setToggleButton] = useState(false);
+  const [isEditItem, setIsEditItem] = useState("");
 
-        const notes = localStorage.getItem("MyNotes");
-        if(notes){
-            return JSON.parse(notes);
-        }
-        else{
-            return []
-        }
+  const [local, setLocal] = useState(false);
+
+  const getLocalInfo = () => {
+    if (typeof window !== "undefined") {
+      const notes = localStorage.getItem("MyNotes");
+      if (notes) {
+        return JSON.parse(notes);
+      }
+    } else {
+      return [];
     }
-  }
-//   const [AllNotes, setAllNotes] = useState(JSON.parse(localStorage.getItem)("MyNotes"));
+  };
   const [AllNotes, setAllNotes] = useState(getLocalInfo());
-
-  useEffect(()=>{
-    localStorage.setItem("MyNotes",JSON.stringify(AllNotes))
-  },[AllNotes]);
+  useEffect(() => {
+    localStorage.setItem("MyNotes", JSON.stringify(AllNotes));
+  }, [AllNotes]);
+  //   const [AllNotes, setAllNotes] = useState(JSON.parse(localStorage.getItem)("MyNotes"));
+  useEffect(() => {
+    if (AllNotes) {
+      setLocal(true);
+    }
+  }, []);
 
   const handleClick = () => {
-    if (!note) alert("cannot add empty node");
+    if (!note) {
+      alert("cannot add empty node");
+    } 
+    else if (note && toggleButton) {
+      
+      setAllNotes(
+        AllNotes.map((cur) => {
+          if (cur.id === isEditItem) {
+            const newNote = {
+              // id: new Date().getTime().toString(),
+              title: note.title,
+              content: note.content,
+              date:new Date().toLocaleString()
+            };
+            return  {...cur,title:note.title,content: note.content,
+              date:new Date().toLocaleString()} ;
+            // return {...cur,newNote}
+          }
+          return cur;
+        })
+      );
+      setNote({
+        title: "",
+        content: "",
+      });
+      setIsEditItem(null);
+      setToggleButton(false);
+    } 
     else {
       const newNote = {
         id: new Date().getTime().toString(),
         title: note.title,
         content: note.content,
+        date: new Date().toLocaleString(),
       };
       setAllNotes([...AllNotes, newNote]);
       setNote({
@@ -53,6 +88,15 @@ const Notes = () => {
       return cur.id !== index;
     });
     setAllNotes(newNotes);
+  };
+
+  const edit = (index) => {
+    const editItem = AllNotes.find((cur) => {
+      return cur.id === index;
+    });
+    setNote({ title: editItem.title, content: editItem.content });
+    setIsEditItem(index);
+    setToggleButton(true);
   };
 
   return (
@@ -84,49 +128,58 @@ const Notes = () => {
           }}
         ></textarea>
         <a className="text-yellow-500 cursor-pointer  " onClick={handleClick}>
-          <FontAwesomeIcon
-            icon={faPlusCircle}
-            className="h-9 -mt-10 hover:scale-125 duration-300"
-          />
+          {!toggleButton ? (
+            <FontAwesomeIcon
+              icon={faPlusCircle}
+              className="h-9 -mt-10 hover:scale-125 duration-300"
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faPenToSquare}
+              className="h-9 -mt-10 hover:scale-125 duration-300"
+            />
+          )}
         </a>
       </div>
-      {(typeof window !== 'undefined') &&
 
-      <div className="flex flex-wrap w-[80%] m-auto">
-        {AllNotes.map((cur) => {
-          return (
-            <div
-              className="whitespace-pre-wrap break-words   relative w-1/4    p-3 m-5 border shadow-lg rounded-lg"
-              key={cur.id}
-            >
-              <p className="text-xl">{cur.title}</p>
-              <hr />
-              <p className="">
-                
-              {cur.content}
-              </p>
-              <div className="absolute right-0 bottom-0 space-x-4 mr-4 ">
-                <a
-                  className="text-red-500 cursor-pointer "
-                  onClick={() => deleteNote(cur.id)}
-                >
-                  <FontAwesomeIcon
-                    icon={faTrash}
-                    className="hover:scale-125 duration-300"
-                  />
-                </a>
-                <a className="text-green-500 cursor-pointer ">
-                  <FontAwesomeIcon
-                    icon={faPenToSquare}
-                    className="hover:scale-125 duration-300"
-                  />
-                </a>
+      <div className="flex justify-center items-baseline flex-wrap w-[80%] m-auto">
+        {local &&
+          AllNotes?.map((cur) => {
+            return (
+              <div
+                className="whitespace-pre-wrap break-words   relative w-1/4    p-3 m-5 border shadow-lg rounded-lg"
+                key={cur.id}
+              >
+                <p className="text-xl">{cur.title}</p>
+                <hr />
+                <p className="mb-4">{cur.content}</p>
+                <div className="flex  space-x-4">
+                  <p className="text-gray-500 text-sm">{cur.date}</p>
+                  <div className="absolute right-0 space-x-4">
+                    <a
+                      className="text-red-500 cursor-pointer "
+                      onClick={() => deleteNote(cur.id)}
+                    >
+                      <FontAwesomeIcon
+                        icon={faTrash}
+                        className="hover:scale-125 duration-300"
+                      />
+                    </a>
+                    <a
+                      className="text-green-500 cursor-pointer "
+                      onClick={() => edit(cur.id)}
+                    >
+                      <FontAwesomeIcon
+                        icon={faPenToSquare}
+                        className="hover:scale-125 duration-300 mr-4"
+                      />
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
-      }
     </>
   );
 };
